9db153ec303970147e254f3ef576c263a766b18c
friendlyantz <friendlyantz@pm.me>
2023-09-20 17:59:03 +1000

    TDD DataBase Service model

---
+++ lib/services/database.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Services
+  class Database
+    attr_reader :data, :schema
+
+    def initialize(data = {}, schema = {})
+      @data = data
+      @schema = schema
+    end
+
+    def add_schema(record:, schema:)
+      @schema[record] = schema
+    end
+
+    def get_record(record:, key:)
+      data.dig(record, key)
+    end
+
+    def add_record(record:, key:, value:)
+      data[record] = {} unless data[record]
+      data[record][key] = value
+    end
+
+    def add_index(record:, paths:, index:)
+      new_record_ids = search_index(record:, paths:).to_a + [index]
+
+      recursive_insert(
+        [record, 'index', *paths],
+        new_record_ids.uniq
+      )
+    end
+
+    def search_index(record:, paths:)
+      data.dig(record, 'index', *paths).to_a
+    end
+
+    def list_records
+      data.keys
+    end
+
+    private
+
+    def recursive_insert(keys, value, trie = data)
+      key = keys.first
+      if keys.length == 1
+        trie[key] = value
+      else
+        trie[key] = {} unless trie[key]
+        recursive_insert(keys.slice(1..-1), value, trie[key])
+      end
+    end
+  end
+end
---
+++ spec/services/database_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+RSpec.describe Services::Database do
+  describe '#add_schema' do
+    it 'adds schema' do
+      db = described_class.new
+      db.add_schema(record: 'some_record', schema: { id: { type: 'String' } })
+      expect(db.schema).to eq({ 'some_record' => { id: { type: 'String' } } })
+    end
+  end
+
+  describe '#get_record' do
+    it 'returns value of the record or nil for non-existebt object or keys' do
+      db = described_class.new({ some_object: { 'column_name' => 777 } })
+      expect(db.get_record(record: :some_object, key: 'column_name')).to eq 777
+      expect(db.get_record(record: 'some_object', key: 'column_name')).to be_nil
+      expect(db.get_record(record: :some_object, key: 2)).to be_nil
+    end
+  end
+
+  describe '#add_record' do
+    context 'when db is empty' do
+      let(:db) { described_class.new({}) }
+
+      it 'add new records' do
+        db.add_record(record: :some_object, key: 'new_key', value: { new: 'key' })
+        expect(db.data).to match({ some_object: { 'new_key' => { new: 'key' } } })
+      end
+    end
+
+    context 'when db has records' do
+      let(:db) { described_class.new(some_object: { 'existing_key' => { existing: 'key' } }) }
+
+      it 'overrides existing data' do
+        db.add_record(record: :some_object, key: 'existing_key', value: { overriden: 'value' })
+        expect(db.data).to match({ some_object: { 'existing_key' => { overriden: 'value' } } })
+      end
+    end
+  end
+
+  describe 'index and search' do
+    describe '#search_index' do
+      it 'returns the value or an empty array when provided record does not exist' do
+        db = described_class
+             .new(some_object: { 'index' => { 'some_key_for_trie' => { 'some_other_key_trie' => [777] } } })
+        expect(db.search_index(record: :some_object, paths: %w[some_key_for_trie some_other_key_trie])).to eq [777]
+        expect(db.search_index(record: :some_object, paths: ['is_admin', false])).to eq []
+        expect(db.search_index(record: :non_existent, paths: %w[some_key_for_trie some_other_key_trie])).to eq []
+      end
+    end
+
+    describe '#add_index' do
+      context 'when initial data is empty' do
+        let(:db) { described_class.new({}) }
+
+        it 'updates the index within the data with the provided record, paths, and index' do
+          db.add_index(record: :some_record, paths: %w[trie_key_one trie_key_two], index: 777)
+          expect(db.data).to match({ some_record: { 'index' => { 'trie_key_one' => { 'trie_key_two' => [777] } } } })
+        end
+      end
+
+      context 'when initial data is not empty' do
+        let(:db) do
+          described_class.new({ some_record: { 'index' => { 'trie_key_one' => { 'trie_key_two' => [25] } } } })
+        end
+
+        it 'append the index within the data with the provided record, paths, and index' do
+          db.add_index(record: :some_record, paths: %w[trie_key_one trie_key_two], index: 777)
+          db.add_index(record: :some_record, paths: %w[trie_key_one another_trie_key_two], index: 888)
+          expect(db.data).to match(
+            { some_record: { 'index' => { 'trie_key_one' =>
+              {
+                'trie_key_two' => [25, 777],
+                'another_trie_key_two' => [888]
+              } } } }
+          )
+        end
+      end
+    end
+
+    describe '#list_records' do
+      it 'returns the keys of the hash data' do
+        expect(described_class.new({
+                                     some_record: { 1 => 777 },
+                                     another_record: { 1 => 777 }
+                                   }).list_records).to eq(%i[some_record another_record])
+      end
+    end
+  end
+end
