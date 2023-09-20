20bfd761df0c3e54ee54d7e18fac7070dcdf8c29
friendlyantz <friendlyantz@pm.me>
2023-09-20 16:31:06 +1000

    ADD Zeitwerk autoloading

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 
 ruby '3.2.2'
 
+gem 'zeitwerk', '~> 2.6'
+
 group :test do
   gem 'rspec', '~> 3.10'
   gem 'super_diff', '~> 0.10.0'
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+unless defined?(Zeitwerk)
+  require 'zeitwerk'
+  loader = Zeitwerk::Loader.new
+  loader.push_dir('lib')
+  loader.push_dir('example-data')
+  loader.setup
+end
+
 class CLI
   def call(arguments: ARGV, out: $stdout, err: $stderr)
     # Your implementation starts here
